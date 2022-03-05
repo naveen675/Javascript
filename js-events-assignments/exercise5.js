d27a9d689311bd54f97621ed0e38a60bb61d3924
Naveen Sai <spnaveen675@gmail.com>
2022-03-06 00:37:34 +0530

    Added exercise 4

---
+++ js-events-assignments/exercise5.js
@@ -0,0 +1,11 @@
+window.addEventListener('load',() => {
+
+    let hexaValue = document.querySelector('.hexa');
+    let col = document.querySelector('.color');
+
+    hexaValue.addEventListener('input', event => {
+
+        let val = event.target.value;
+
+    })
+})
